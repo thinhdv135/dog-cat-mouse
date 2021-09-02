37fe7fb57e0df59913139966da0e9889ca2594f5
Thinh <doanthinh1305@gmail.com>
2021-09-02 17:23:08 +0700

    sayHi method

--- Dog.js
+++ Dog.js
@@ -1,9 +1,16 @@
-function Dog() {
-	this.stomach= = [];
+var chalk = require('chalk');
+
+function Dog(name) {
+	this.stomach = [];
+	this.name = name;
 }
 
 Dog.prototype.eat= function(cat) {
 	this.stomach.push(cat);	
+};
+
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am '+ chalk.red(this.name))
 };
 
 module.exports = Dog;
